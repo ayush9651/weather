504e3a63b32c1734c33a0aa6a057d3882fa755c0
ayush9651 <138026521+ayush9651@users.noreply.github.com>
2026-03-16 13:29:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 async function getWeather(){
 
-let city = document.getElementById("city").value;
+let city=document.getElementById("city").value;
 
-let apiKey = "b47d969d51d93c22b7a23e4bf451ec58";
+let apiKey="b47d969d51d93c22b7a23e4bf451ec58";
 
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-let response = await fetch(url);
+let response=await fetch(url);
 
-let data = await response.json();
+let data=await response.json();
 
-document.getElementById("result").innerHTML =
-`Temperature: ${data.main.temp}°C <br>
-Weather: ${data.weather[0].description}`;
-
+document.getElementById("result").innerHTML=
+`
+<h3>${data.name}</h3>
+<p>🌡 Temperature: ${data.main.temp}°C</p>
+<p>☁ Weather: ${data.weather[0].description}</p>
+`;
 }
